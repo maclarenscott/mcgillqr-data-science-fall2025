fd79b11554524afd077a422cd400687fa868b2ae
Danyl Satanovskyi <danylsat26@gmail.com>
2025-11-14 15:51:00 -0500

    simple stuff to start

--- superSimpleCLOBUpdates.py
+++ superSimpleCLOBUpdates.py
@@ -9,23 +9,23 @@ event_url = f"https://gamma-api.polymarket.com/events/slug/{slug}"
 
 event_data = requests.get(event_url).json() #event data as a json
 
-event_id = event_data['id'] #id of # of event
+event_id = event_data['id'] #id of event
 
 markets = event_data['markets'] #all markets for that event
 
-pardon_diddy_yes_token = '93425298759467682711704940688772441010955525019881668068016025097366958962368'
-pardon_diddy_no_token = '110772069756724332116648059385969433526059191442232057731782827132431473437246'
+pardon_diddy_yes_token = '93425298759467682711704940688772441010955525019881668068016025097366958962368' #id of yes token
+pardon_diddy_no_token = '110772069756724332116648059385969433526059191442232057731782827132431473437246' #id of no token
 
 wanted_tokens = [pardon_diddy_yes_token, pardon_diddy_no_token]
 #market_IDs = [market['id'] for market in markets] #IDs of markets for given event
 
 async def get_polymarket_data():
-    ''' continuously gives order book updates'''
-    async with websockets.connect(clob_URL, ping_interval=10) as ws: #establish connection to endpoint
+    ''' continuously gives order book updates and print them to terminal'''
+    async with websockets.connect(clob_URL, ping_interval=10) as ws: #establish connection to endpoint with pings to maintain it
         await ws.send(json.dumps({'assets_ids': wanted_tokens, 'type': 'market'})) #what we want
         while True:
-            data = await ws.recv()
-            d = json.loads(data)
+            data = await ws.recv() #data we receive
+            d = json.loads(data) 
             print(d)
 
 asyncio.run(get_polymarket_data())
